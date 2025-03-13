ba3158c938d5f120cde5ffefdfaadad73847ebe6
Rajdeep Dutta <rajdeepdutta70@gmail.com>
2025-03-13 11:21:52 +0530

    creating, modifying and range in array

---
+++ array1.rb
@@ -0,0 +1,18 @@
+# arr = Array.new(4, "Hello")
+
+# print "#{arr}\n"
+
+
+arr = ["hello", [45, 3], "end"]
+# print arr[0, 2]
+# print arr[1, 3]
+
+# arr[0] = "world"
+
+# arr_new = arr
+# arr_new[0] = "world"
+# puts arr_new
+# puts arr
+
+puts arr[1..2]
+puts arr [1...2]
