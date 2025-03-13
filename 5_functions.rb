aa72858720546c3a8f69c19f538b3c4065634777
Rajdeep Dutta <rajdeepdutta70@gmail.com>
2025-03-13 17:32:06 +0530

    function syntax

---
+++ 5_functions.rb
@@ -0,0 +1,67 @@
+# def func 
+#     return "Tom"
+# end
+
+# text = func
+# puts "#{text} is returned"
+
+
+# def new_func 
+#     name = gets.chomp
+#     puts "#{name} is the new text"
+# end
+
+# new_func()
+# text = new_func
+
+
+# ***************************
+
+
+# def fibonacci()
+#     puts "Enter number:"
+
+#     num = gets.chomp.to_i
+#     puts "\n"
+#     a = 0
+#     b = 1
+#     c = 0
+
+# for i in 0..num
+#     puts a
+#     c = a + b
+#     a = b
+#     b = c
+#     break if a > num
+# end
+# end
+
+# fibonacci()
+
+
+
+
+# *************************
+
+
+# def calculator 
+#     puts "Enter 2 digits: "
+#     num1 = gets.chomp.to_i
+#     num2 = gets.chomp.to_i
+
+#     puts "Enter add, sub, divide, multiply: "
+#     operation = gets.chomp
+#     case operation
+#     when "add" then puts "Sum is: #{num1 + num2}"
+#     when "sub" then puts "Difference is: #{num1 - num2}"
+#     when "divide" then puts "Quotient is: #{num1 / num2}  and remainder is: #{num1 % num2}"
+#     when "multiply" then puts "Product is: #{num1 * num2}"
+#     end
+# end
+
+# calculator()
+
+
+
+# ******************************
+
