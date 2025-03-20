165bdfee2226791eb754753ce7efc3070f65ee48
Rajdeep Dutta <rajdeepdutta70@gmail.com>
2025-03-20 17:59:58 +0530

    add loading data form json feature

--- 9_practice_ruby.rb
+++ 9_practice_ruby.rb
@@ -1,158 +1,171 @@
 # EXPENSE TRACKER
 
 
-# require 'json'
+require 'json'
 
-# class Transaction
-#     attr_accessor :amount, :type, :category, :date
-#     def initialize(amount, type, category, date)
-#         @amount = amount
-#         @type = type
-#         @category = category
-#         @date = date
-#     end
-#     def transaction_info
-#         puts "#{@date} | #{@type} | #{@category} | #{@amount}"
-#     end
-# end
+class Transaction
+    attr_accessor :amount, :type, :category, :date
+    def initialize(amount, type, category, date)
+        @amount = amount
+        @type = type
+        @category = category
+        @date = date
+    end
+    def transaction_info
+        puts "#{@date} | #{@type} | #{@category} | #{@amount}"
+    end
+end
 
-# class Account
-#     attr_accessor :balance, :transactions
-#     def initialize
-#         @balance = 0
-#         @transactions = []
-#     end
+class Account
+    attr_accessor :balance, :transactions
+    def initialize
+        @balance = 0
+        @transactions = []
+        load_from_file("transactions.json")
+    end
 
-#     def add_transaction(transaction)
-#         @transactions << transaction
-#         update_balance()
-#     end
+    def add_transaction(transaction)
+        @transactions << transaction
+        update_balance()
+    end
 
-#     def remove_transaction(transaction)
-#         @transactions.delete(transaction)
-#         update_balance()
-#     end
-#     def update_balance
-#         @balance = @transactions.reduce(0) {|sum, transaction|
-#         transaction.type == "income" ? sum + transaction.amount : sum - transaction.amount    
-#     }
-#     end
-#     def view_transactions
-#         @transactions.each {|x| puts x.transaction_info}
-#         # puts "Your balance is: #{@balance}"
-#     end
+    def remove_transaction(transaction)
+        @transactions.delete(transaction)
+        update_balance()
+    end
+    def update_balance
+        @balance = @transactions.reduce(0) {|sum, transaction|
+        transaction.type == "income" ? sum + transaction.amount : sum - transaction.amount    
+    }
+    end
+    def view_transactions
+        @transactions.each {|x| puts x.transaction_info}
+        # puts "Your balance is: #{@balance}"
+    end
 
-#     def total_income
-#         total = 0
-#         @transactions.each {|t|
-#         if t.type == "income"
-#             total += t.amount
-#         end
-#         }
-#         total
-#     end
+    def total_income
+        total = 0
+        @transactions.each {|t|
+        if t.type == "income"
+            total += t.amount
+        end
+        }
+        total
+    end
 
-#     def total_expense
-#         expense = 0
-#         @transactions.each {|t|
-#         if t.type != "income"
-#             expense += t.amount
-#         end
-#         }
-#         expense
-#     end
+    def total_expense
+        expense = 0
+        @transactions.each {|t|
+        if t.type != "income"
+            expense += t.amount
+        end
+        }
+        expense
+    end
 
-#     def generate_report
-#         puts "Your total income is: #{total_income}"
-#         puts "Your total expense is: #{total_expense}"
-#         puts "Your balance: #{@balance}"
-#     end
-#     def save_to_file(filename)
-#         data = @transactions.map { |t| { amount: t.amount, category: t.category, type: t.type, date: t.date } }
-#         File.write(filename, JSON.dump(data))
-#     end
-
-# end
+    def generate_report
+        puts "Your total income is: #{total_income}"
+        puts "Your total expense is: #{total_expense}"
+        puts "Your balance: #{@balance}"
+    end
+    def save_to_file(filename)
+        data = @transactions.map { |t| { amount: t.amount, category: t.category, type: t.type, date: t.date } }
+        File.write(filename, JSON.dump(data))
+    end
 
 
-# class Expense_Tracker
-#     attr_accessor :account
-#     def initialize
-#         @account = Account.new()
-#     end
+    def load_from_file(filename)
+        puts "loading..."
+        if File.exist?(filename)
+            file_data = File.read(filename)
+            puts file_data
+            data = JSON.parse(file_data)
+            puts data
+            @transactions = data.map {|i| Transaction.new(i["date"], i["type"], i["category"], i["amount"])}
+        end
+    end
 
-#     def menu
-#         loop do 
-#             puts "Personal Expense Tracker"
-#             puts "1. Add Income"
-#             puts "2. Add Expense"
-#             puts "3. Remove Transaction"
-#             puts "4. View Transactions"
-#             puts "5. Generate Report"
-#             puts "6. Exit"
-#             print "Choose an option: "
+end
 
-#             choice = gets.chomp().to_i
-#             case choice
-#             when 1
-#                 add_transaction("income")
-#             when 2
-#                 add_transaction("expense")
-#             when 3
-#                 remove_transaction
-#             when 4
-#                 view_transactions
-#             when 5
-#                 generate_report
-#             when 6
-#                 puts "Goodbye!"
-#                 break
-#             else
-#                 puts "Incorrect selection"
-#             end
-#             @account.save_to_file('transactions.json')
-#         end
 
-#     end
+class Expense_Tracker
+    attr_accessor :account
+    def initialize
+        @account = Account.new()
+    end
 
-#     def add_transaction(type)
-#         print "Enter amount: $"
-#         amount = gets.chomp.to_f
-#         print "Enter category: "
-#         category = gets.chomp
-#         print "Enter date (YYYY-MM-DD): "
-#         date = gets.chomp
+    def menu
+        loop do 
+            puts "Personal Expense Tracker"
+            puts "1. Add Income"
+            puts "2. Add Expense"
+            puts "3. Remove Transaction"
+            puts "4. View Transactions"
+            puts "5. Generate Report"
+            puts "6. Exit"
+            print "Choose an option: "
 
-#         transaction = Transaction.new(amount, type, category, date)
-#         @account.add_transaction(transaction)
-#         puts "Transaction added!"
-#         puts "\n"
-#     end
+            choice = gets.chomp().to_i
+            case choice
+            when 1
+                add_transaction("income")
+            when 2
+                add_transaction("expense")
+            when 3
+                remove_transaction
+            when 4
+                view_transactions
+            when 5
+                generate_report
+            when 6
+                puts "Goodbye!"
+                break
+            else
+                puts "Incorrect selection"
+            end
+            @account.save_to_file('transactions.json')
+        end
 
-#     def remove_transaction
-#         puts "Select to remove:"
-#         @account.view_transactions
-#         print "Enter the transaction number to remove: "
-#         index = gets.chomp().to_i - 1 
-#         transaction = @account.transactions[index]
-#         @account.remove_transaction(transaction)
-#         puts "Transaction removed"
-#     end
+    end
 
-#     def view_transactions
-#         puts "All Transactions"
-#         @account.view_transactions
-#     end
-#     def generate_report
-#         @account.generate_report()
-#     end
+    def add_transaction(type)
+        print "Enter amount: $"
+        amount = gets.chomp.to_f
+        print "Enter category: "
+        category = gets.chomp
+        print "Enter date (YYYY-MM-DD): "
+        date = gets.chomp
+
+        transaction = Transaction.new(amount, type, category, date)
+        @account.add_transaction(transaction)
+        puts "Transaction added!"
+        puts "\n"
+    end
+
+    def remove_transaction
+        puts "Select to remove:"
+        @account.view_transactions
+        print "Enter the transaction number to remove: "
+        index = gets.chomp().to_i - 1 
+        transaction = @account.transactions[index]
+        @account.remove_transaction(transaction)
+        puts "Transaction removed"
+    end
+
+    def view_transactions
+        puts "All Transactions"
+        @account.view_transactions
+    end
+    def generate_report
+        @account.generate_report()
+    end
 
     
-# end
+end
 
 
-# tracker = Expense_Tracker.new
-# tracker.menu()
+tracker = Expense_Tracker.new
+tracker.menu()
 
 
 # **********************************
@@ -162,110 +175,110 @@
 
 
 
-require "json"
+# require "json"
 
-class Note
-    attr_accessor :note
-    def initialize(note)
-        @note = note
-    end
+# class Note
+#     attr_accessor :note
+#     def initialize(note)
+#         @note = note
+#     end
 
-    def note_info
-        @note
-    end
-end
+#     def note_info
+#         @note
+#     end
+# end
 
-class List
-    attr_accessor :notes
-    def initialize
-        @notes = []
-        load_from_file("test.json")
-    end
-    def add_to_list(note)
-        @notes << note
-    end
-    def remove_from_list(note)
-        @notes.delete(note)
-    end
+# class List
+#     attr_accessor :notes
+#     def initialize
+#         @notes = []
+#         load_from_file("test.json")
+#     end
+#     def add_to_list(note)
+#         @notes << note
+#     end
+#     def remove_from_list(note)
+#         @notes.delete(note)
+#     end
 
-    def view_list
-        puts "Your Notes"
-        @notes.each {|i| puts i.note_info}
-    end
+#     def view_list
+#         puts "Your Notes"
+#         @notes.each {|i| puts i.note_info}
+#     end
 
-    def save_to_file(filename)
-        data = @notes.map {|i| {note: i.note}}
-        File.write(filename, JSON.dump(data))
+#     def save_to_file(filename)
+#         data = @notes.map {|i| {note: i.note}}
+#         File.write(filename, JSON.dump(data))
 
-    end
+#     end
 
-    def load_from_file(filename)
-        if File.exist?(filename)
-          file_data = File.read(filename)
-          notes_array = JSON.parse(file_data)
-          @notes = notes_array.map { |data| Note.new(data['note']) }
-        end
-      end
-end
+#     def load_from_file(filename)
+#         if File.exist?(filename)
+#           file_data = File.read(filename)
+#           notes_array = JSON.parse(file_data)
+#           @notes = notes_array.map { |data| Note.new(data['note']) }
+#         end
+#       end
+# end
 
-class Create_Note
-    def initialize
-        @list = List.new
-    end
+# class Create_Note
+#     def initialize
+#         @list = List.new
+#     end
 
-    def menu
-                loop do 
-                    puts "Note"
-                    puts "1. Add Note"
-                    puts "2. Remove Note"
-                    puts "3. View Notes"
-                    puts "0. Exit"
-                    print "Choose an option: "
+#     def menu
+#                 loop do 
+#                     puts "Note"
+#                     puts "1. Add Note"
+#                     puts "2. Remove Note"
+#                     puts "3. View Notes"
+#                     puts "0. Exit"
+#                     print "Choose an option: "
         
-                    choice = gets.chomp().to_i
-                    case choice
-                    when 1
-                        add_note
-                    when 2
-                        remove_note
-                    when 3
-                        view_note
-                    when 0
-                        puts "Good bye!"
-                        break
-                    else
-                        puts "Incorrect selection"
-                    end
-                    @list.save_to_file("test.json")
-                end
+#                     choice = gets.chomp().to_i
+#                     case choice
+#                     when 1
+#                         add_note
+#                     when 2
+#                         remove_note
+#                     when 3
+#                         view_note
+#                     when 0
+#                         puts "Good bye!"
+#                         break
+#                     else
+#                         puts "Incorrect selection"
+#                     end
+#                     @list.save_to_file("test.json")
+#                 end
         
-            end
+#             end
 
-    def add_note
-        puts "Your note: "
-        text = gets.chomp().to_s
+#     def add_note
+#         puts "Your note: "
+#         text = gets.chomp().to_s
 
-        note = Note.new(text)
-        @list.add_to_list(note)
-        puts "Your note is added!"
-        puts "\n"
-    end
-    def remove_note
-        puts "Select your note to remove"
-        @list.view_list()
-        print "Select the number to remove: "
-        index = gets.chomp().to_i - 1
-        note = @list.notes[index]
-        @list.remove_from_list(note)
-        puts "Your note is removed!"
-        puts "\n"
-    end
-    def view_note
-        @list.view_list
-        puts "\n"
-    end
-end
+#         note = Note.new(text)
+#         @list.add_to_list(note)
+#         puts "Your note is added!"
+#         puts "\n"
+#     end
+#     def remove_note
+#         puts "Select your note to remove"
+#         @list.view_list()
+#         print "Select the number to remove: "
+#         index = gets.chomp().to_i - 1
+#         note = @list.notes[index]
+#         @list.remove_from_list(note)
+#         puts "Your note is removed!"
+#         puts "\n"
+#     end
+#     def view_note
+#         @list.view_list
+#         puts "\n"
+#     end
+# end
 
 
-note = Create_Note.new()
-note.menu()
+# note = Create_Note.new()
+# note.menu()
